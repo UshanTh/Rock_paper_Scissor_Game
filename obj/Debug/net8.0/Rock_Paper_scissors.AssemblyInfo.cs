6c54f6e1e5b2bd2c266cb92c3f8b9ba375987d28
UshanTh <ushantharindu11@gmail.com>
2024-06-16 16:30:10 +0530

    Initial Commit

--- obj/Debug/net8.0/Rock_Paper_scissors.AssemblyInfo.cs
+++ obj/Debug/net8.0/Rock_Paper_scissors.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rock_Paper_scissors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f70be3ad1116eb9644a7a24ba0318d80762babf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rock_Paper_scissors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rock_Paper_scissors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
